cc1791a9b68a61d5eda3b7a90b2deb2f0df17d9a
Selviraja <aryan.webdevelopmentprojects@gmail.com>
2021-04-05 22:04:10 +0530

    NodeJS - Globals

---
+++ nodejs_1_globals.js
@@ -0,0 +1,9 @@
+//GLOBALS(Global variables) - No Window
+
+//__dirname - path to current directory
+//__filename - filename
+//require - function to use modules(CommonJS)
+//module - info about the current module(file)
+//process - info about env where the program is being executed
+
+//setInterval() and setTimeout()
