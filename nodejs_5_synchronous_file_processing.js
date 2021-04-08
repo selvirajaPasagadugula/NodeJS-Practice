ea822d10b14f34f2ef22d874405c62148e532e84
Selviraja <aryan.webdevelopmentprojects@gmail.com>
2021-04-08 21:06:48 +0530

    NodeJS - synchronous file processing

---
+++ nodejs_5_synchronous_file_processing.js
@@ -0,0 +1,11 @@
+const { readFileSync, writeFileSync } = require("fs");
+
+const first_file = readFileSync("./files/file1.txt", "utf8");
+const second_file = readFileSync("./files/file2.txt", "utf8");
+
+writeFileSync(
+  "./files/result.txt",
+  `Here is the result:\n${first_file}\n${second_file}\n\n`,
+  { flag: "a" }
+);
+console.log(first_file);
